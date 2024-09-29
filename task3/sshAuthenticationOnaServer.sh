c1639a5e52e003ae15797d157d98e7e4b913f99c
AralAskarov <askarov.10.09@gmail.com>
2024-09-29 17:16:06 +0500

    3 task script that configures SSH key-based authentication on a remote server.

---
+++ task3/sshAuthenticationOnaServer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+password=$1
+
+# its for my virutal machine
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+	ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
+fi
+
+sshpass -p "$password" ssh-copy id -o StrictHostKeyChecking=no  -i ~/.ssh/id_rsa.pub $askarov@192.168.0.103
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no askarov@192.168.0.103 "chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys"
+ssh -v askarov@192.168.0.103
+
