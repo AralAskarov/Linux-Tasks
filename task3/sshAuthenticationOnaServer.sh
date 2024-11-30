2e00a25f9066fc4e1bf44e9b1d8fb20361ca4203
AralAskarov <askarov.10.09@gmail.com>
2024-11-30 05:32:34 +0500

    finalim

--- task3/sshAuthenticationOnaServer.sh
+++ task3/sshAuthenticationOnaServer.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 password=$1
-
+username=$2
+host_ip=$3
 # for virtual machine
 
 if [ ! -f ~/.ssh/id_rsa ]; then
 	ssh-keygen -t rsa -b 4096 -N "" -f ~/.ssh/id_rsa
 fi
 
-sshpass -p "$password" ssh-copy id -o StrictHostKeyChecking=no  -i ~/.ssh/id_rsa.pub $askarov@192.168.0.103
-sshpass -p "$password" ssh -o StrictHostKeyChecking=no askarov@192.168.0.103 "chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys"
-ssh -v askarov@192.168.0.103
+sshpass -p "$password" ssh-copy id -o StrictHostKeyChecking=no  -i ~/.ssh/id_rsa.pub $username@$host_ip
+sshpass -p "$password" ssh -o StrictHostKeyChecking=no $username@$host_ip "chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys"
+ssh -v $username@$host_ip
 
